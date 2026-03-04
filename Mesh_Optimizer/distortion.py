6835c2e539b155ebca4911cd09f23a1792c19668
darkfluoz <65126033+darkfluoz@users.noreply.github.com>
2026-03-05 00:24:36 +0100

    Update distortion.py

--- Mesh_Optimizer/distortion.py
+++ Mesh_Optimizer/distortion.py
@@ -1,21 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-A = np.array([[1, -np.sqrt(3.) / 3], [0, 2 * np.sqrt(3.) / 3]],dtype=float)
+# Matriu de mapeig per al triangle equilàter de referència
+W = np.array([[1.0, -np.sqrt(3.0) / 3.0], 
+              [0.0,  2.0 * np.sqrt(3.0) / 3.0]], dtype=float)
 
-def calculaDistorsioTriangle(Xe):
-  etat = 0.0
-  Dphi = np.array([Xe[1,:]-Xe[0,:], Xe[2,:]-Xe[0,:]]).T
-  Dphi = Dphi@A
-  detDphi = np.linalg.det(Dphi)
-  frobDphi = np.linalg.norm(Dphi,'fro')
-  etat = (frobDphi**2)/(2*abs(detDphi))
-  return etat
+def distorsio_element(coords_tri):
+    # Vectorització dels eixos del triangle
+    J = np.array([coords_tri[1, :] - coords_tri[0, :], 
+                  coords_tri[2, :] - coords_tri[0, :]]).T
+    
+    Dphi = J @ W
+    
+    # Optimització: determinant i norma de Frobenius explícita (més ràpid per matrius 2x2)
+    det_Dphi = Dphi[0, 0] * Dphi[1, 1] - Dphi[0, 1] * Dphi[1, 0]
+    frob_sq = np.sum(Dphi**2) 
+    
+    return (frob_sq**2) / (2.0 * abs(det_Dphi))
 
-def calculaDistorsioMalla(X, T):
-    eta = 0.0
-    for e in range (T.shape[0]): #0,...,Nint-1 són els nodes interiors
-      Xe = X[T[e,:],:]
-      eta += calculaDistorsioTriangle(Xe)**2
-    return np.sqrt(eta)
-
+def distorsio_total(X_nodes, T_elements):
+    dist_sq_sum = 0.0
+    for element in T_elements:
+        coords = X_nodes[element, :]
+        dist_sq_sum += distorsio_element(coords)**2
+        
+    return np.sqrt(dist_sq_sum)
